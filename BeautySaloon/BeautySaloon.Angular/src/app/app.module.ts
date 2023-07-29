2df1ea727cd3a61b349c868965a10d61f4fd7a06
DaniilBabiak <imperatorvkresle790@gmail.com>
2023-07-29 16:20:19 +0200

    Added a front-page-photo

--- BeautySaloon/BeautySaloon.Angular/src/app/app.module.ts
+++ BeautySaloon/BeautySaloon.Angular/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FrontPageComponent } from './front-page/front-page/front-page.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 
 
