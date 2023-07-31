e5443410735080d7d344ba20b3d96ce278911d8a
Yevhen Nesterenko <yevhen.nesterenko@dataart.com>
2023-08-01 01:16:57 +0300

    Configured indetity server

--- BeautySaloon/BeautySaloon.Identity/Migrations/20230731134133_Added fullName to user.Designer.cs
+++ BeautySaloon/BeautySaloon.Identity/Migrations/20230731134133_Added fullName to user.Designer.cs
@@ -4,6 +4,7 @@ using BeautySaloon.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BeautySaloon.Identity.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230731134133_Added fullName to user")]
+    partial class AddedfullNametouser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
