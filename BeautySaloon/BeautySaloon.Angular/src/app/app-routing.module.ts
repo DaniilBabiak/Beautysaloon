899011b260516c9a66081fe96188401423e7a61e
DaniilBabiak <imperatorvkresle790@gmail.com>
2023-08-08 23:45:58 +0200

    test-fix

--- BeautySaloon/BeautySaloon.Angular/src/app/app-routing.module.ts
+++ BeautySaloon/BeautySaloon.Angular/src/app/app-routing.module.ts
@@ -1,27 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
-
 const routes: Routes = [
 
   {path: 'user', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule)},
   { path: 'welcome', loadChildren: () => import('./views/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'auth-callback', loadChildren: () => import('./views/auth-callback/auth-callback.module').then(m => m.AuthCallbackModule) },
   {path: "**", redirectTo: 'welcome'},
-
-  // { path: '', redirectTo: 'user', pathMatch: 'full' },
-  // { path: 'welcome', component: FrontPageComponent },
-  //
-  //
-  //
-  // { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then((m) => m.AdminModule) },
-  // { path: 'user', loadChildren: () => import('./modules/user/user.module').then((m) => m.UserModule) },
-  // { path: 'profile', component: ProfileComponent },
-  // { path: 'auth-callback', component: AuthCallbackComponent },
-  // { path: 'test', component: TestRestComponent }
 
 ];
 
+// @ts-ignore
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
