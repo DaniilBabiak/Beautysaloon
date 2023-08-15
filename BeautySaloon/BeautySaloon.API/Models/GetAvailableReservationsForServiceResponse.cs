4b47d037f114daf115dca65ad0fd5a9c4a1afc12
Yevhen Nesterenko <yevhen.nesterenko@dataart.com>
2023-08-15 19:04:39 +0300

    Added reservation service and global exception handler

---
+++ BeautySaloon/BeautySaloon.API/Models/GetAvailableReservationsForServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿using BeautySaloon.API.Entities.BeautySaloonContextEntities;
+
+namespace BeautySaloon.API.Models;
+
+public class GetAvailableReservationsForServiceResponse
+{
+    public Service Service { get; set; }
+    public IEnumerable<DateTime> AvailableReservations { get; set; } = new List<DateTime>();
+}
---
+++ BeautySaloon/BeautySaloon.API/Services/Interfaces/IReservationService.cs
@@ -0,0 +1,10 @@
+﻿using BeautySaloon.API.Entities.BeautySaloonContextEntities;
+
+namespace BeautySaloon.API.Services.Interfaces;
+
+public interface IReservationService
+{
+    Task<Reservation> CreateReservationAsync(Reservation reservation);
+    Task<List<Reservation>> GetAvailableReseravtionsByServiceId(int serviceId);
+    Task<List<Reservation>> GetReservationsByServiceId(int serviceId);
+}
