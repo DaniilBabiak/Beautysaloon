1968d503c17b5acf3e55cf9c3f3dd54dfe320e50
Yevhen Nesterenko <yevhen.nesterenko@dataart.com>
2023-08-16 22:08:52 +0300

    Added customer area

---
+++ BeautySaloon/BeautySaloon.API/Areas/Customer/Controllers/ReservationController.cs
@@ -0,0 +1,47 @@
+﻿using BeautySaloon.API.Entities.BeautySaloonContextEntities;
+using BeautySaloon.API.Models;
+using BeautySaloon.API.Services.Interfaces;
+using Duende.IdentityServer.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautySaloon.API.Areas.Customer.Controllers;
+
+public class ReservationController : CustomerControllerBase
+{
+    private readonly IReservationService _reservationService;
+
+    public ReservationController(IReservationService reservationService)
+    {
+        _reservationService = reservationService;
+    }
+
+    [HttpGet("getAvailable/{serviceId}")]
+    public async Task<IActionResult> GetAvailableReservationsForServiceAsync(int serviceId)
+    {
+        var availableReservations = await _reservationService.GetAvailableReseravtionsByServiceId(serviceId);
+
+        var result = new GetAvailableReservationsForServiceResponse
+        {
+            Service = availableReservations.First()?.Service,
+            AvailableReservations = availableReservations.Select(r => r.DateTime)
+        };
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateReservationAsync(CreateReservationRequest createReservationRequest)
+    {
+        var customerId = User.Identity.Name;
+        var reservation = new Reservation
+        {
+            ServiceId = createReservationRequest.ServiceId,
+            CustomerId = customerId,
+            DateTime = createReservationRequest.DateTime
+        };
+
+        var result = await _reservationService.CreateReservationAsync(reservation);
+        return Ok(result);
+    }
+}
