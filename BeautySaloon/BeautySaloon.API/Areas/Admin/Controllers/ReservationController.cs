634a81cd833d9602d643b33cee4b62f4b9f41489
Yevhen Nesterenko <yevhen.nesterenko@dataart.com>
2023-08-16 22:08:45 +0300

    Added admin area

---
+++ BeautySaloon/BeautySaloon.API/Areas/Admin/Controllers/ReservationController.cs
@@ -0,0 +1,34 @@
+﻿using BeautySaloon.API.Entities.BeautySaloonContextEntities;
+using BeautySaloon.API.Models;
+using BeautySaloon.API.Services.Interfaces;
+using Duende.IdentityServer.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautySaloon.API.Areas.Admin.Controllers;
+public class ReservationController : AdminControllerBase
+{
+    private readonly IReservationService _reservationService;
+
+    public ReservationController(IReservationService reservationService)
+    {
+        _reservationService = reservationService;
+    }
+
+    [HttpGet("getAll")]
+    [Authorize("admin")]
+    public async Task<IActionResult> GetAllReservationsAsync()
+    {
+        var result = await _reservationService.GetAllReservationsAsync();
+        return Ok(result);
+    }
+
+    [HttpGet("getAll/{serviceId}")]
+    [Authorize("admin")]
+    public async Task<IActionResult> GetAllReservationsForServiceAsync(int serviceId)
+    {
+        var result = await _reservationService.GetReservationsByServiceId(serviceId);
+
+        return Ok(result);
+    }
+}
---
+++ BeautySaloon/BeautySaloon.API/Areas/Admin/Controllers/ServiceCategoryController.cs
@@ -0,0 +1,45 @@
+﻿using BeautySaloon.API.Entities.BeautySaloonContextEntities;
+using BeautySaloon.API.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautySaloon.API.Areas.Admin.Controllers;
+
+public class ServiceCategoryController : AdminControllerBase
+{
+    private readonly IServiceCategoryService _category;
+
+    public ServiceCategoryController(IServiceCategoryService category)
+    {
+        _category = category;
+    }
+
+    [HttpPost]
+    [Authorize("admin")]
+    public async Task<IActionResult> CreateCategoryAsync([FromBody] ServiceCategory category)
+    {
+        var test = User;
+        var result = await _category.CreateCategoryAsync(category);
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    [Authorize("admin")]
+    public async Task<IActionResult> UpdateCategoryAsync([FromBody] ServiceCategory category)
+    {
+        var result = await _category.UpdateCategoryAsync(category);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize("admin")]
+    public async Task<IActionResult> DeleteCategoryAsync(int id)
+    {
+        await _category.DeleteCategoryAsync(id);
+
+        return Ok();
+    }
+}
